77ce23e619f535517cab4c598fa3f68806fce91e
akhilesh_verma <146834811+akhileshverma92@users.noreply.github.com>
2025-01-23 23:12:22 +0530

    Delete uiorbit directory

--- uiorbit/src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-theme/dist/provider"
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
